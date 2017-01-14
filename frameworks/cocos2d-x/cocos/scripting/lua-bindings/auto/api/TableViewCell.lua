bc7433e3a4a066ffe88356da0e304cbd2b718135
lcy <406195930@qq.com>
2017-01-14 12:16:56 +0800

    add the frameworks cocos file

---
+++ frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/api/TableViewCell.lua
@@ -0,0 +1,35 @@
+
+--------------------------------
+-- @module TableViewCell
+-- @extend Node
+-- @parent_module cc
+
+--------------------------------
+-- Cleans up any resources linked to this cell and resets <code>idx</code> property.
+-- @function [parent=#TableViewCell] reset 
+-- @param self
+        
+--------------------------------
+-- The index used internally by SWTableView and its subclasses
+-- @function [parent=#TableViewCell] getIdx 
+-- @param self
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- 
+-- @function [parent=#TableViewCell] setIdx 
+-- @param self
+-- @param #int uIdx
+        
+--------------------------------
+-- 
+-- @function [parent=#TableViewCell] create 
+-- @param self
+-- @return TableViewCell#TableViewCell ret (return value: cc.TableViewCell)
+        
+--------------------------------
+-- 
+-- @function [parent=#TableViewCell] TableViewCell 
+-- @param self
+        
+return nil
